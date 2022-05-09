e529959e9a4439cd61ad53bb1c60a32f9a2fda82
INA <codechemi@gmail.com>
2022-05-10 02:12:55 +0900

    Add : Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import "./App.css";
 
 function App() {
+
+  let Banner = () =>{
+    return(
+      <div>
+        안녕하세요?
+      </div>
+    )
+  }
+
+  let Table = () =>{
+    return(
+      <div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Banner></Banner>
+        <Table></Table>
+      </header>
     </div>
   );
 }
